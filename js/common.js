0e31d2fa1694d5948796f6452c2e0edcbaac734b
ren_maeda <mcreate@gmail.com>
2020-06-12 14:04:25 +0900

    カフェサイト初期リリース

---
+++ js/common.js
@@ -0,0 +1,11 @@
+$('.bxslider').bxSlider({
+            auto: true,
+            autoControls: false, //再生ボタンみたいなの
+            stopAutoOnClick: false,
+            pager: true,
+            slideWidth: 952,
+            mode: "fade",
+            controls:false,
+            pager: true,//下の丸ぽち
+//            mode:"vertical",//縦に
+        });
